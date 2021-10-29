0df9978d5ca95c13f834f393fe612eec0d08d4c2
Andrea Falconi <andrea.falconi@gmail.com>
2021-10-29 17:12:10 +0200

    use own kubectl on malaga node to make flux work.

--- shell.nix
+++ shell.nix
@@ -8,14 +8,14 @@ let
   version = {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "f6ccdfcd2ac4f2e259d20e378737dcbd0ca7debe";                 # (1)
-    sha256 = "1d2lk7a0l166pvgy0xfdlhxgja986hgn39szn9d1fqamyhxzvbaz";  # (2)
+    rev = "f6ccdfcd2ac4f2e259d20e378737dcbd0ca7debe";
+    sha256 = "1d2lk7a0l166pvgy0xfdlhxgja986hgn39szn9d1fqamyhxzvbaz";
   };
   nixpin = fetchFromGitHub version;
   pinnedPkgs = import nixpin {};
 
   tools = with pinnedPkgs; {
-    inherit git fluxcd;
+    inherit kubectl fluxcd;
   };
 
 in pinnedPkgs.mkShell {
